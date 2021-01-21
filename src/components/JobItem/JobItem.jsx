62b5108a64368e07ef665e6847cf2512f84503a5
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2021-01-21 16:37:17 +0100

    Change name on job to data

--- src/components/JobItem/JobItem.jsx
+++ src/components/JobItem/JobItem.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import * as S from "./styled";
 
-const JobItem = ({ id, job }) => {
+const JobItem = ({ id, data }) => {
   return (
     <S.Container>
       <div key={id}>
-        <img height="100" src={job.company_logo} alt="logo" />
-        <h2>{job.company}</h2>
-        <strong>{job.type}</strong>
-        <a>{job.url}</a>
+        <img height="100" src={data.company_logo} alt={data.company} />
+        <h2>{data.company}</h2>
+        <strong>{data.type}</strong>
+        <a href={data.url}>{data.url}</a>
         {/* <p>{job.description}</p> */}
       </div>
     </S.Container>
