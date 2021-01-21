5eaddb7ea311cf108bc182f287ca3639770d067d
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2021-01-21 11:48:58 +0100

    Create HomePage and add Switch with Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/" component={HomePage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
