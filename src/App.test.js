fa979ec27762d34452cb18f2eaa1da7112130149
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2021-01-27 00:20:20 +0100

    Refactor test for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { shallow } from "enzyme";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Test App.js", () => {
+  it("contains an H1 with application name", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("h1").text()).toBe("Workfinder");
+  });
 });
