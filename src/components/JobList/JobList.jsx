4fdb076131ea87d0e5d7c5c25b2ea6b46ef0ef95
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2021-01-27 12:03:25 +0100

    Add search results found paragraph

--- src/components/JobList/JobList.jsx
+++ src/components/JobList/JobList.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import JobItem from "../JobItem/JobItem";
+import { JobListContext } from "../../contexts/JobListContextProvider";
 import * as S from "./styled";
 
 const JobList = ({ jobList }) => {
+  const { searchResult } = useContext(JobListContext);
   return (
     <S.Container>
+      {jobList && jobList.length !== 0 ? (
+        <S.SearchResults>
+          Search results for "{searchResult.searchTerm}"
+        </S.SearchResults>
+      ) : (
+        ""
+      )}
       {jobList.length === 0 ? (
         <p>No search results found</p>
       ) : (
